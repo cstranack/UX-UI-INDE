8296a20da8dd6d7c945d6fdf54189ffcf4ac1aab
Claire Stranack <44362052+cstranack@users.noreply.github.com>
2019-10-18 15:06:06 +0100

    updates

--- www/js/script.js
+++ www/js/script.js
@@ -1,21 +1,43 @@
-/*const btn = document.querySelector('.btn');
 
 
-findOutMore.addEventListener("mouseenter", (e) => {
-    btn.classList.add("bounceInRight");
-    //you can hide elements untill an event 
-    btn.classList.remove("hide");
+const bear = document.getElementById('bear'); 
+const happyBear = document.getElementById('happyBear');
+// const bearContainer = document.getElementsByClassName("bearContainer");
+
+
+bear.addEventListener("mouseenter", (e) => {
+  console.log("hover");
+  bear.style.visibility = 'hidden'; 
 })
 
-findOutMore.addEventListener("mouseleave", (e) => {
-    btn.classList.remove("pinkBTN");
+bear.addEventListener("mouseleave", (e) => {
+  console.log("unhover");
+  bear.style.visibility = 'visible'; 
 })
 
-//many different events|| e= event 
-btn.addEventListener('click', (e) =>{
-    console.log('clicked');
-    console.log(e);
-})
-*/
 
 
+//Tabs for child learn page
+
+function openSection(evt, tabName) {
+    console.log("working");
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+ 
