4f56e3fd4ff2642a929cc202196280d296829ac6
Claire Stranack <44362052+cstranack@users.noreply.github.com>
2019-10-20 00:01:24 +0100

    adding details

--- www/js/script.js
+++ www/js/script.js
@@ -19,6 +19,7 @@ bear.addEventListener("mouseleave", (e) => {
 
 //Tabs for child learn page
 
+
 function openSection(evt, tabName) {
     console.log("working");
     // Declare all variables
@@ -29,15 +30,30 @@ function openSection(evt, tabName) {
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-  
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
- 
+}
+
+// animated bounce on buttons
+const btn =  document.querySelector('.btn');
+const btn1 =  document.querySelector('.btn1');
+
+  btn.addEventListener("mouseenter", (e) => {
+  btn.classList.add('animated', 'bounce')
+})
+btn.addEventListener("mouseleave", (e) => {
+  btn.classList.remove('animated', 'bounce')
+})
+
+  btn1.addEventListener("mouseenter", (e) => {
+  btn1.classList.add('animated', 'bounce')
+})
+btn1.addEventListener("mouseleave", (e) => {
+  btn1.classList.remove('animated', 'bounce')
+})
